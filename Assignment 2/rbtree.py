96fbd77fd31e8a6db6b1066215068c7360496737
bhishma <matib275@gmail.com>
2020-02-24 14:29:13 +0530

    implemented queries and also spaces > tabs ;)

--- Assignment 2/rbtree.py
+++ Assignment 2/rbtree.py
@@ -7,193 +7,206 @@ BLACK = 1
 
 class Node:
 
-	# 0 - red , 1 - black 
-	def __init__(self, val, left=None, right=None, parent=None, size=1, color=RED):
-		self.val = val
-		self.left = left
-		self.right = right
-		self.parent = parent
-		self.size = size
-		self.color = color
+    # 0 - red , 1 - black 
+    def __init__(self, val, left=None, right=None, parent=None, size=1, color=RED):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.parent = parent
+        self.size = size
+        self.color = color
 
 
 class RedBlackTree():
 
-	global NIL
-	NIL = Node(val=-1, color=BLACK, size=0)
+    global NIL
+    NIL = Node(val=-1, color=BLACK, size=0)
 
 
-	def check(curr):
-		if curr != NIL:
-			assert curr.size == 1 + curr.left.size + curr.right.size
-			l = RedBlackTree.check(curr.left)
-			r = RedBlackTree.check(curr.right)
-			assert l == r
-			return l + (1 if curr.color == BLACK else 0)
-		else:
-			return 0
+    def check(curr):
+        if curr != NIL:
+            assert curr.size == 1 + curr.left.size + curr.right.size
+            l = RedBlackTree.check(curr.left)
+            r = RedBlackTree.check(curr.right)
+            assert l == r
+            return l + (1 if curr.color == BLACK else 0)
+        else:
+            return 0
 
 
-	def check_tree(self):
-		print("Checking ... ")
-		assert self.root.color == BLACK, "Root isn't black"
-		assert RedBlackTree.check(self.root)
-		print("Looks fine !")
+    def check_tree(self):
+        print("Checking ... ")
+        assert self.root.color == BLACK, "Root isn't black"
+        assert RedBlackTree.check(self.root)
+        print("Looks fine !")
 
-	def print_tree(self):
-		print('\n'.join(RedBlackTree.pretty_print("", True, [], self.root)))
-		# RedBlackTree.inorder_print(self.root)
+    def print_tree(self):
+        print('\n'.join(RedBlackTree.pretty_print("", True, [], self.root)))
+        # RedBlackTree.inorder_print(self.root)
 
-	def pretty_print(prefix, isTail, sb, root):
-		if root == NIL:
-			return ["Tree is empty"]
+    def pretty_print(prefix, isTail, sb, root):
+        if root == NIL:
+            return ["Tree is empty"]
 
-		if root.right != NIL:
-			RedBlackTree.pretty_print(prefix + ("|   " if isTail else "    "), False, sb, root.right)
+        if root.right != NIL:
+            RedBlackTree.pretty_print(prefix + ("|   " if isTail else "    "), False, sb, root.right)
 
-		# sb.append(prefix + "|-- " + str(root.val) + " (p = {}, c = {}, sz = {})".format(root.parent.val, root.color, root.size))
-		sb.append(prefix + "|-- " + str(root.val))
-		if root.left != NIL:
-			RedBlackTree.pretty_print(prefix + ("|   " if not isTail else "    "), True, sb, root.left)
+        # sb.append(prefix + "|-- " + str(root.val) + " (p = {}, c = {}, sz = {})".format(root.parent.val, root.color, root.size))
+        sb.append(prefix + "|-- " + str(root.val))
+        if root.left != NIL:
+            RedBlackTree.pretty_print(prefix + ("|   " if not isTail else "    "), True, sb, root.left)
 
-		return sb
+        return sb
 
-	def update_size(node):
-		node.size = 1 + node.left.size + node.right.size
+    def update_size(node):
+        node.size = 1 + node.left.size + node.right.size
 
+    def count_less_than(curr, x):
+        if curr == NIL:
+            return 0
+        elif x <= curr.val:
+            return RedBlackTree.count_less_than(curr.left, x)
+        else:
+            return 1 + curr.left.size + RedBlackTree.count_less_than(curr.right, x)
 
+    def get_count_in_range(self, l, r):
+        return RedBlackTree.count_less_than(self.root, r + 1) - RedBlackTree.count_less_than(self.root, l)
 
-	def fixup(self, curr):
-		while curr.parent.color == RED:
-			print(curr.val, "inside fix")
-			if curr.parent == curr.parent.parent.left:
-				y = curr.parent.parent.right 	# uncle of curr
-				if y.color == RED:
-					curr.parent.color = BLACK
-					y.color = BLACK
-					curr.parent.parent.color = RED
-					curr = curr.parent.parent
-				else:
-					if curr == curr.parent.right:	# uncle is not red and curr is right child
-						curr = curr.parent
-						self.left_rotate(curr)
+    def fixup(self, curr):
+        while curr.parent.color == RED:
+            print(curr.val, "inside fix")
+            if curr.parent == curr.parent.parent.left:
+                y = curr.parent.parent.right    # uncle of curr
+                if y.color == RED:
+                    curr.parent.color = BLACK
+                    y.color = BLACK
+                    curr.parent.parent.color = RED
+                    curr = curr.parent.parent
+                else:
+                    if curr == curr.parent.right:   # uncle is not red and curr is right child
+                        curr = curr.parent
+                        self.left_rotate(curr)
 
-					curr.parent.color = BLACK
-					curr.parent.parent.color = RED
-					self.right_rotate(curr.parent.parent)
-			else:
-				y = curr.parent.parent.left
-				if y.color == RED:
-					curr.parent.color = BLACK
-					y.color = BLACK
-					curr.parent.parent.color = RED
-					curr = curr.parent.parent
-				else:
-					if curr == curr.parent.left:
-						curr = curr.parent
-						self.right_rotate(curr)
+                    curr.parent.color = BLACK
+                    curr.parent.parent.color = RED
+                    self.right_rotate(curr.parent.parent)
+            else:
+                y = curr.parent.parent.left
+                if y.color == RED:
+                    curr.parent.color = BLACK
+                    y.color = BLACK
+                    curr.parent.parent.color = RED
+                    curr = curr.parent.parent
+                else:
+                    if curr == curr.parent.left:
+                        curr = curr.parent
+                        self.right_rotate(curr)
 
-					curr.parent.color = BLACK
-					curr.parent.parent.color = RED
-					self.left_rotate(curr.parent.parent)
+                    curr.parent.color = BLACK
+                    curr.parent.parent.color = RED
+                    self.left_rotate(curr.parent.parent)
 
-		self.root.color = BLACK
+        self.root.color = BLACK
 
-	def b_height(curr):
-		h = 0
-		while curr != NIL:
-			h += 1 if curr.color == BLACK else 0
-			curr = curr.left # leftist ! 
+    def b_height(curr):
+        h = 0
+        while curr != NIL:
+            h += 1 if curr.color == BLACK else 0
+            curr = curr.left # leftist ! 
 
-		return h
+        return h
 
-	def get_height(self):
-		return RedBlackTree.b_height(self.root)
+    def get_height(self):
+        return RedBlackTree.b_height(self.root)
 
-	def insert(self, x):
-		# self.root = RedBlackTree.insert_(self, self.root, x)
+    def insert(self, x):
+        # self.root = RedBlackTree.insert_(self, self.root, x)
 
-		# self.root.parent = NIL
-		y = NIL
-		new_node = Node(val=x, left=NIL, right=NIL)
-		curr = self.root
-		while curr != NIL:
-			y = curr
-			curr = curr.left if x < curr.val else curr.right
+        # self.root.parent = NIL
+        y = NIL
+        new_node = Node(val=x, left=NIL, right=NIL)
+        curr = self.root
+        while curr != NIL:
+            y = curr
+            curr = curr.left if x < curr.val else curr.right
 
-		new_node.parent = y
-		if y == NIL:
-			self.root = new_node
-		elif new_node.val == y.val:
-			return
-		elif new_node.val < y.val:
-			y.left = new_node
-		else:
-			y.right = new_node
+        new_node.parent = y
+        if y == NIL:
+            self.root = new_node
+        elif new_node.val == y.val:
+            return
+        elif new_node.val < y.val:
+            y.left = new_node
+        else:
+            y.right = new_node
 
-		curr = new_node.parent
-		while curr != NIL:
-			RedBlackTree.update_size(curr)
-			curr = curr.parent
+        curr = new_node.parent
+        while curr != NIL:
+            RedBlackTree.update_size(curr)
+            curr = curr.parent
 
-		self.fixup(new_node)
-		print(new_node.val)
-		self.print_tree()
-		print("Height ", self.get_height())
-		self.check_tree()
+        self.fixup(new_node)
+        print(new_node.val)
+        self.print_tree()
+        print("Height ", self.get_height())
+        self.check_tree()
 
-	# Need to update b_height, cnt and all other fields !
-
-
-
-	def left_rotate(self, x):
-		y = x.right
-		x.right = y.left
-		if y.left != NIL:
-			y.left.parent = x
-		y.parent = x.parent
-
-		if x.parent == NIL:
-			self.root = y
-		elif x == x.parent.left:
-			x.parent.left = y
-		else:
-			x.parent.right = y
-
-		y.left = x
-		x.parent = y
-		RedBlackTree.update_size(x)
-		RedBlackTree.update_size(y)
-
-	def right_rotate(self, y):
-		x = y.left
-		y.left = x.right
-		if x.right != NIL:
-			x.right.parent = y
-		x.parent = y.parent
-
-		if y.parent == NIL:
-			self.root = x
-		elif y == y.parent.right:
-			y.parent.right = x
-		else:
-			y.parent.left = x
-
-		x.right = y
-		y.parent = x
-		RedBlackTree.update_size(y)
-		RedBlackTree.update_size(x)
+    # Need to update b_height, cnt and all other fields !
 
 
 
-	def __init__(self, root=None):
-		self.root = NIL
+    def left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+        if y.left != NIL:
+            y.left.parent = x
+        y.parent = x.parent
+
+        if x.parent == NIL:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+
+        y.left = x
+        x.parent = y
+        RedBlackTree.update_size(x)
+        RedBlackTree.update_size(y)
+
+    def right_rotate(self, y):
+        x = y.left
+        y.left = x.right
+        if x.right != NIL:
+            x.right.parent = y
+        x.parent = y.parent
+
+        if y.parent == NIL:
+            self.root = x
+        elif y == y.parent.right:
+            y.parent.right = x
+        else:
+            y.parent.left = x
+
+        x.right = y
+        y.parent = x
+        RedBlackTree.update_size(y)
+        RedBlackTree.update_size(x)
+
+
+
+    def __init__(self, root=None):
+        self.root = NIL
 
 
 
 
 rbtree = RedBlackTree()
 for i in range(20):
-	rbtree.insert(i)
+    rbtree.insert(i)
 
 rbtree.print_tree()
+l = 10
+r = 15
+
+print("count in range %d to %d" % (l, r), rbtree.get_count_in_range(l, r))
